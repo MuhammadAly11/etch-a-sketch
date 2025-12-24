1a4a2f5a36aca6c58a4e76881c7c1061807e12a1
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2025-12-24 21:06:59 +0200

    Remove unnecessary grid function

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const gridContainer = document.querySelector(".grid-container");
 
 function createGrid(length, width) {
   const numOfGrids = width ? length * width : length * length;
-  const grid = [];
   for (let i = 0; i < numOfGrids; i++) {
     const div = document.createElement("div");
     div.classList.add("cell");
     div.addEventListener("mouseout", () => {
       div.classList.add("hovered-cell");
     });
-    grid.push(div);
     gridContainer.appendChild(div);
   }
 }
