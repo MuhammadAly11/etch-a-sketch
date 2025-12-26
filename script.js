faf1a94e0a231100e6d59df55cf937e1fcfff61f
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2025-12-26 02:09:35 +0200

    Support changing cells number
    Almost all the logic and structure of code changed
    while implementing this feature

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-const CELL_LENGTH = 10;
-
-function createGrid(length, width) {
+function createGrid(width, length, numOfCellsPerSide) {
+  const grid = getGridState();
   resetGrid();
+
+  width = width ? width : grid.width;
+  length = length ? length : width;
+  numOfCellsPerSide = numOfCellsPerSide
+    ? numOfCellsPerSide
+    : Math.sqrt(grid.totalCells);
+
+  const totalCells = numOfCellsPerSide ** 2;
+  const cellLength = width / numOfCellsPerSide;
+
   const gridContainer = document.querySelector(".grid-container");
-  width = width ? width : length;
-  gridContainer.style.width = width * CELL_LENGTH + "px";
-  gridContainer.style.height = length * CELL_LENGTH + "px";
-  const numOfGrids = length * width;
-  for (let i = 0; i < numOfGrids; i++) {
+  gridContainer.style.width = width + "px";
+  gridContainer.style.height = length + "px";
+
+  for (let i = 0; i < totalCells; i++) {
     const div = document.createElement("div");
     div.classList.add("cell");
+    div.style.width = cellLength + "px";
+    div.style.height = cellLength + "px";
     div.addEventListener("mouseout", () => {
       div.classList.add("hovered-cell");
     });
     gridContainer.appendChild(div);
   }
+  console.log(getGridState());
 }
 
-createGrid(16);
+createGrid(300, undefined, 50);
 
-const newGrid = document.querySelector(".new-grid");
-newGrid.addEventListener("click", () => {
+const gridSizeBtn = document.querySelector(".grid-size");
+gridSizeBtn.addEventListener("click", () => {
   let size = +prompt("Enter new grid size: ");
   createGrid(size);
+});
+
+const cellNumBtn = document.querySelector(".cell-number");
+cellNumBtn.addEventListener("click", () => {
+  let cellNum = 0;
+  while (!cellNum || cellNum < 1 || cellNum > 100) {
+    cellNum = +prompt("Enter number of cells per side (max: 100):");
+  }
+  createGrid(undefined, undefined, cellNum);
 });
 
 function resetGrid() {
@@ -33,4 +53,19 @@ function resetGrid() {
   gridContainer = document.createElement("div");
   gridContainer.classList.add("grid-container");
   document.body.appendChild(gridContainer);
+}
+
+function getGridState() {
+  const gridContainer = document.querySelector(".grid-container");
+  const cell = document.querySelector(".cell");
+  if (!gridContainer || !cell) {
+    return undefined;
+  }
+
+  return {
+    width: +gridContainer.style.width.slice(0, -2),
+    height: +gridContainer.style.height.slice(0, -2),
+    cellLength: +cell.style.width.slice(0, -2),
+    totalCells: gridContainer.getElementsByTagName("div").length,
+  };
 }
